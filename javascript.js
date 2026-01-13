4be9bf43686c86a5f68f698a82b662feb3cffecd
gamitsejal123 <sejalgamit285@gmail.com>
2026-01-13 10:52:25 +0530

    Initial project commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+// Hamburger Menu Toggle
+// const hamburger = document.getElementById("hamburger");
+// const navMenu = document.getElementById("navMenu");
+
+// hamburger.addEventListener("click", () => {
+//   navMenu.classList.toggle("active");
+// });
+
