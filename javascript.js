c6b82c7248a9b2fc051b90e63acf38fda145e594
gamitsejal123 <sejalgamit285@gmail.com>
2026-01-17 11:47:31 +0530

    add another pages

--- javascript.js
+++ javascript.js
@@ -1,8 +1,46 @@
 // Hamburger Menu Toggle
-// const hamburger = document.getElementById("hamburger");
-// const navMenu = document.getElementById("navMenu");
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
 
-// hamburger.addEventListener("click", () => {
-//   navMenu.classList.toggle("active");
-// });
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+}); 
+
+//slide-btn
+   const wrapper = document.querySelector(".card-wrapper");
+  const leftBtn = document.querySelector(".left-btn");
+  const rightBtn = document.querySelector(".right-btn");
+
+  // ✅ Initial state: RIGHT button active
+  rightBtn.classList.add("active");
+  leftBtn.classList.remove("active");
+
+  function getScrollAmount() {
+    const card = wrapper.querySelector(".card");
+    return card.offsetWidth + 30;
+  }
+
+  rightBtn.addEventListener("click", () => {
+    wrapper.scrollBy({
+      left: getScrollAmount(),
+      behavior: "smooth"
+    });
+
+    rightBtn.classList.add("active");
+    leftBtn.classList.remove("active");
+  });
+
+  leftBtn.addEventListener("click", () => {
+    wrapper.scrollBy({
+      left: -getScrollAmount(),
+      behavior: "smooth"
+    });
+
+    leftBtn.classList.add("active");
+    rightBtn.classList.remove("active");
+  });
+
+
+
+
 
